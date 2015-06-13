07b626e13f1f5571adbd776dfa8d67f3d8a89339
Shaboku <shaboku@outlook.com>
2015-06-13 16:58:04 -0300

    A punto de que llegue el id al archivo de modificar

---
+++ controles/f_modificar.php
@@ -0,0 +1,10 @@
+<?php 
+	if($_POST)
+	{
+		echo "ALELUYA";
+	}
+?>
+<section id="modificar" data-archivo-id="">
+	xd
+	<?php $xd = implode($_POST); echo $xd;	?>
+</section>
--- vista/menu.php
+++ vista/menu.php
@@ -1,7 +1,7 @@
 <section id="menu">
 	<span id="pico"></span>
 	<ul>
-		<li><button><span><?= url_type('img', 'modificararchivo.svg'); ?></span>Modificar</button></li>
+		<li><button data-form="modificar"><span><?= url_type('img', 'modificararchivo.svg'); ?></span>Modificar</button></li>
 		<li><button><span><?= url_type('img', 'renombrararchivo.svg'); ?></span>Renombrar</button></li>
 		<li><button><span><?= url_type('img', 'tipoarchivo.svg'); ?></span>Cambiar tipo de archivo</button></li>
 		<li><button><span><?= url_type('img', 'borrararchivo.svg'); ?></span>Borrar</button></li>
